c2338c7ca3b05801d56c26985b2f6fb07065992d
unknown <tsstr1973@yahoo.co.jp>
2018-01-21 13:44:55 +0900

    ファイル追加

---
+++ HelloXamarin/HelloXamarinPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace HelloXamarin
+{
+    public partial class HelloXamarinPage : ContentPage
+    {
+        public HelloXamarinPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
